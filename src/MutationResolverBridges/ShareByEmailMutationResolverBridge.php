d161a28fc9b4e508c73f5b04e5a51e485e55d7dd
leoloso <leo@getpop.org>
2021-10-04 09:14:29 +0000

    Merge pull request #1116 from leoloso/make-autowire-functions-final
    Made all autowire functions final

--- src/MutationResolverBridges/ShareByEmailMutationResolverBridge.php
+++ src/MutationResolverBridges/ShareByEmailMutationResolverBridge.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace PoPSitesWassup\ShareMutations\MutationResolverBridges;
 
-use Symfony\Contracts\Service\Attribute\Required;
 use PoP\ComponentModel\MutationResolvers\MutationResolverInterface;
-use PoPSitesWassup\ShareMutations\MutationResolvers\ShareByEmailMutationResolver;
 use PoPSitesWassup\FormMutations\MutationResolverBridges\AbstractFormComponentMutationResolverBridge;
+use PoPSitesWassup\ShareMutations\MutationResolvers\ShareByEmailMutationResolver;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class ShareByEmailMutationResolverBridge extends AbstractFormComponentMutationResolverBridge
 {
     protected ShareByEmailMutationResolver $shareByEmailMutationResolver;
 
     #[Required]
-    public function autowireShareByEmailMutationResolverBridge(
+    final public function autowireShareByEmailMutationResolverBridge(
         ShareByEmailMutationResolver $shareByEmailMutationResolver,
     ): void {
         $this->shareByEmailMutationResolver = $shareByEmailMutationResolver;
